b0b0e03017ee28ee7793e17f58984dfc599b815c
Pranavan <pranavanyanala@gmail.com>
2024-04-13 16:48:07 +0530

    created login and register endpoints in the server

--- root/server.js
+++ root/server.js
@@ -5,14 +5,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const mongoUrl = 'mongodb://localhost:27017/Users';
 
 // MongoDB connection
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(mongoUrl)
 .then(() => {
     console.log('MongoDB connected');
 })
@@ -30,7 +27,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Handle POST request from register.html form
-app.post('/register', (req, res) => {
+app.post('/register',(req, res) => {
     // Create a new User instance with the data from the request body
     const newUser = new User({
         name: req.body.name,
@@ -45,13 +42,33 @@ app.post('/register', (req, res) => {
         .then(() => {
             // Respond with a success message
             res.send('User registered successfully');
+            res.status(201);
         })
         .catch(err => {
             // Respond with an error message
             res.status(400).send('Error registering user');
-            console.error('Error registering user:', err);
+            res.send('Error registering user:', err);
+            console.log(err);
         });
 });
+
+app.post("/login",(req,res)=>{
+    User.findOne({email:req.body.email}).then(user=>{
+        if(user.password===req.body.password){
+            res.status(200).json(user);
+        }
+        else{
+            res.send("Incorrect credentials").status(401);
+        }
+    }).catch(err=>{
+        res.send("server error")
+        console.log(err);    
+    });
+})
+
+app.get("/users",(req,res)=>{
+    User.find().then(users => res.status(200).json(users));
+})
 
 // Start the server
 app.listen(port, () => {
